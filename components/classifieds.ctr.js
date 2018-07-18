80eb39ec9f373251b281da15e47614638dd17eb6
Matt Freitas <mattfreitas415@gmail.com>
2018-07-17 22:25:43 -0700

    added ability to edit classifieds

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -4,11 +4,17 @@
 
    angular
       .module("ngClassifieds")
-      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
 
          classifiedsFactory.getClassifieds().then(function(classifieds) {
              $scope.classifieds = classifieds.data;
          });  
+
+         var contact = {
+             name: "matt Freitas",
+             phone: "(555) 555-5555",
+             email: "matt@matttt.com"
+         }
 
          $scope.openSideBar = function() {
              $mdSidenav('left').open();
@@ -16,6 +22,38 @@
 
          $scope.closeSideBar = function() {
              $mdSidenav('left').close();
+         }
+
+         $scope.saveClassified = function(classified) {
+             if(classified) {
+                classified.contact = contact;
+                $scope.classifieds.push(classified);
+                $scope.classified = {};
+                $scope.closeSideBar();
+                showToast("Classified saved!");
+             }
+         }
+
+         $scope.editClassified = function(classified) {
+             $scope.editing = true; 
+             $scope.openSideBar(); 
+             $scope.classified = classified; 
+         }
+
+         $scope.saveEdit = function() {
+             $scope.editing = false;
+             $scope.classified = {};
+             $scope.closeSideBar();
+             showToast("Edited!");
+         }
+
+         function showToast(message) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .content(message)
+                    .position('top, right')
+                    .hideDelay(3000)
+            );
          }
 
       });
