b8ec3a9c034d18c49d3c276c5e5e996f176f3173
Matt Freitas <mattfreitas415@gmail.com>
2018-07-28 13:17:04 -0700

    fixed routing and added watchers

--- components/classifieds/classifieds.ctr.js
+++ components/classifieds/classifieds.ctr.js
@@ -1,100 +1,86 @@
 (function() {
 
-   "use strict";
-
-   angular
-      .module("ngClassifieds")
-      .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast, $mdDialog) {
-
-         var vm = this;
-
-         vm.openSideBar = openSideBar;
-         vm.closeSideBar = closeSideBar;
-         vm.saveClassified = saveClassified;
-         vm.editClassified = editClassified;
-         vm.saveClassified = saveClassified;
-         vm.deleteClassified = deleteClassified;
-
-         vm.classifieds;
-         vm.categories;
-         vm.editing;
-         vm.classified;
-
-         classifiedsFactory.getClassifieds().then(function(classifieds) {
-             vm.classifieds = classifieds.data;
-             vm.categories = getCategories(vm.classifieds);
-         });  
-
-         var contact = {
-             name: "matt Freitas",
-             phone: "(555) 555-5555",
-             email: "matt@matttt.com"
-         }
-
-         function openSideBar() {
-             $mdSidenav('left').open();
-         }
-
-         function closeSideBar() {
-             $mdSidenav('left').close();
-         }
-
-         function saveClassified(classified) {
-             if(classified) {
-                classified.contact = contact;
-                $scope.classifieds.push(classified);
-                $scope.classified = {};
-                $scope.closeSideBar();
-                showToast("Classified saved!");
-             }
-         }
-
-         function editClassified(classified) {
-             vm.editing = true; 
-             openSideBar(); 
-             vm.classified = classified; 
-         }
-
-         function saveEdit() {
-             vm.editing = false;
-             vm.classified = {};
-             closeSideBar();
-             showToast("Edited!");
-         }
-
-         function deleteClassified(event, classified) {
-             var confirm = $mdDialog.confirm()
-                .title('Are you sure you want to delete ' + classified.title + '?')
-                .ok('Yes')
-                .cancel('No')
-                .targetEvent(event);
-            $mdDialog.show(confirm).then(function() {
-                var index = $scope.classifieds.indexOf(classified);
-                vm.classifieds.splice(index, 1);
-            }, function() {
-                
-            }); 
-         }
-
-         function showToast(message) {
-            $mdToast.show(
-                $mdToast.simple()
-                    .content(message)
-                    .position('top, right')
-                    .hideDelay(3000)
-            );
-         }
-
-         function getCategories(classifieds) {
-             var categories = [];
-
-             angular.forEach(classifieds,function(item) {
-                angular.forEach(item.categories, function(category){
-                    categories.push(category);
-                });
-             });
-             return _.uniq(categories);
-         }
-
+    "use strict";
+  
+    angular
+      .module('ngClassifieds')
+      .controller('classifiedsController', function($scope, $mdToast, $mdSidenav, $mdDialog, $state, $stateParams, classifiedsFactory) {
+  
+        var vm = this;
+  
+        vm.openSidebar = openSidebar;
+        vm.editClassified = editClassified;
+        vm.deleteClassified = deleteClassified;
+        vm.showSearchBar = false;
+        vm.showFilters = false;
+  
+        classifiedsFactory.getClassifieds().then(function(data) {
+          vm.classifieds = data.data;
+          vm.categories = getCategories(vm.classifieds);
+        });
+  
+        $scope.$on('newClassified', function(event, data) {
+          data.id = vm.classifieds.length + 1;
+          vm.classifieds.push(data);
+          showToast('Classified Saved');
+        });
+  
+        $scope.$on('editSaved', function(event, message) {
+          showToast(message);
+        });
+  
+        vm.sidebarTitle;
+  
+        function showToast(message) {
+          $mdToast.show(
+            $mdToast.simple()
+              .content(message)
+              .position('top, right')
+              .hideDelay(3000)
+          );
+        }
+  
+        function openSidebar() {
+          vm.sidebarTitle = 'Add a Classified';
+          $state.go('classifieds.new');
+        }
+  
+        function editClassified(classified) {
+          vm.editing = true;
+          vm.sidebarTitle = 'Edit Classified';
+          vm.classified = classified;
+          $state.go('classifieds.edit', { id: classified.id, classified: classified });
+        }
+  
+        function deleteClassified(event, classified) {
+          var confirm = $mdDialog.confirm()
+              .title('Are you sure you want to delete ' + classified.title + '?')
+              .targetEvent(event)
+              .ok('Yes')
+              .cancel('No');
+          $mdDialog.show(confirm).then(function() {
+            console.log(event);
+            var index = vm.classifieds.indexOf(classified);
+            vm.classifieds.splice(index, 1);
+            showToast('Classified Deleted');
+          }, function() {
+            vm.status = classified.title + ' is still here.';
+          });
+        }
+  
+        function getCategories(classifieds) {
+  
+          var categories = [];
+  
+          angular.forEach(classifieds, function(ad) {
+            angular.forEach(ad.categories, function(category) {
+              categories.push(category);
+            });
+          });
+  
+          return _.uniq(categories);
+        }
+  
       });
-})();
+  
+  })();
